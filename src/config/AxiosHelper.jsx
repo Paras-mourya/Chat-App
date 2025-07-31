3ba4ef45418934c2a358a24fb3ca70a81b8bf1e4
Paras-mourya <parasmourya288@gmail.com>
2025-08-01 00:40:13 +0530

    forts message

--- src/config/AxiosHelper.jsx
+++ src/config/AxiosHelper.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const baseURL="http://localhost:8080";
-export const httpClient=axios.create({
-    baseURL:baseURL,
-})
+export const baseURL = import.meta.env.VITE_BACKEND_URL;
+
+export const httpClient = axios.create({
+  baseURL: baseURL,
+});
