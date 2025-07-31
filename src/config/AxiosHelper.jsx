7c67ef97893dadbb2426cad7798a3828f2e7dba6
Paras-mourya <parasmourya288@gmail.com>
2025-08-01 02:18:39 +0530

    firstmessage

--- src/config/AxiosHelper.jsx
+++ src/config/AxiosHelper.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// Automatically pick from .env or .env.production
 export const baseURL = import.meta.env.VITE_BACKEND_URL;
 
 export const httpClient = axios.create({
